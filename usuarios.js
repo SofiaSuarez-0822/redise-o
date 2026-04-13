af2b30d40d3de5c10bddce7b84315481c6d03110
SofiaSuarez-0822 <sofiasuarez035@gmail.com>
2026-04-13 11:41:12 -0500

    Add files via upload

---
+++ usuarios.js
@@ -0,0 +1,16 @@
+const usuarios = [
+  {
+    nombre: "Sofia Suarez",
+    correo: "sofia@email.com",
+    password: "1234",
+    rol: "admin"
+    
+  }, 
+  {
+    nombre: "Juan Perez",
+    correo: "juan@email.com",
+    password: "abcd",
+    rol: "user"
+    
+  }
+];
